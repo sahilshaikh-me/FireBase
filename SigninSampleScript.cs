b81d222c3ffb038302215fd00ddfe875428e79bd
sahillshaikh1 <50543009+sahillshaikh1@users.noreply.github.com>
2021-08-07 20:32:28 +0530

    Add files via upload

---
+++ SigninSampleScript.cs
@@ -0,0 +1,112 @@
+﻿
+namespace SignInSample {
+  using System;
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using Google;
+  using UnityEngine;
+  using UnityEngine.UI;
+    using Photon.Pun;
+    using Photon.Realtime;
+  public class SigninSampleScript : MonoBehaviourPun {
+        public static SigninSampleScript Instance { get; set; }
+        public Text statusText;
+
+    private string webClientId = "484090280329-88r6h5q67odjudka6l7gc0thkd4c6sp7.apps.googleusercontent.com";
+
+    private GoogleSignInConfiguration configuration;
+        public Uri ImageUri;
+      
+        void Awake() {
+      configuration = new GoogleSignInConfiguration {
+            WebClientId = webClientId,
+            RequestIdToken = true
+      };
+    }
+        private void Update()
+        {
+            if(Instance== null)
+            {
+                Instance = this;
+            }
+        }
+        public void OnSignIn() {
+      GoogleSignIn.Configuration = configuration;
+      GoogleSignIn.Configuration.UseGameSignIn = false;
+      GoogleSignIn.Configuration.RequestIdToken = true;
+      AddStatusText("Calling SignIn");
+
+      GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+        OnAuthenticationFinished);
+    }
+
+    public void OnSignOut() {
+      AddStatusText("Calling SignOut");
+      GoogleSignIn.DefaultInstance.SignOut();
+    }
+
+    public void OnDisconnect() {
+      AddStatusText("Calling Disconnect");
+      GoogleSignIn.DefaultInstance.Disconnect();
+    }
+
+    internal void OnAuthenticationFinished(Task<GoogleSignInUser> task) {
+      if (task.IsFaulted) {
+        using (IEnumerator<System.Exception> enumerator =
+                task.Exception.InnerExceptions.GetEnumerator()) {
+          if (enumerator.MoveNext()) {
+            GoogleSignIn.SignInException error =
+                    (GoogleSignIn.SignInException)enumerator.Current;
+            AddStatusText("Got Error: " + error.Status + " " + error.Message);
+          } else {
+            AddStatusText("Got Unexpected Exception?!?" + task.Exception);
+          }
+        }
+      } else if(task.IsCanceled) {
+        AddStatusText("Canceled");
+      } else if(task.IsCompleted) {
+
+                PhotonNetwork.LocalPlayer.NickName = task.Result.DisplayName;
+                PhotonNetwork.LoadLevel("HUB");
+                ImageUri = task.Result.ImageUrl;
+                statusText.text = "Success";
+                //AddStatusText("Welcome: " + task.Result.DisplayName + "!" + task.Result.ImageUrl);
+            }
+    }
+
+    public void OnSignInSilently() {
+      GoogleSignIn.Configuration = configuration;
+      GoogleSignIn.Configuration.UseGameSignIn = false;
+      GoogleSignIn.Configuration.RequestIdToken = true;
+      AddStatusText("Calling SignIn Silently");
+
+      GoogleSignIn.DefaultInstance.SignInSilently()
+            .ContinueWith(OnAuthenticationFinished);
+    }
+
+
+    public void OnGamesSignIn() {
+      GoogleSignIn.Configuration = configuration;
+      GoogleSignIn.Configuration.UseGameSignIn = true;
+      GoogleSignIn.Configuration.RequestIdToken = false;
+
+      AddStatusText("Calling Games SignIn");
+
+      GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+        OnAuthenticationFinished);
+    }
+
+    public List<string> messages = new List<string>();
+    void AddStatusText(string text) {
+      if (messages.Count == 5) {
+        messages.RemoveAt(0);
+      }
+      messages.Add(text);
+      string txt = "";
+      foreach (string s in messages) {
+        txt += "\n" + s;
+      }
+      statusText.text = txt;
+    }
+  }
+}
